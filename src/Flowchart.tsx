fd0fa36d49de8dd9e260b1353d6317a1c1fb5e84
Michael Abrahams <miabraha@gmail.com>
2024-01-02 15:18:16 -0500

    Import drag and drop example with Sidebar
    Add LoadBalancer, Server, Database, Cache, Client nodes

--- src/Flowchart.tsx
+++ src/Flowchart.tsx
@@ -1,50 +1,100 @@
+import { useState, useRef, useCallback } from 'react';
 import ReactFlow, {
-  Controls,
-  Background,
-  MiniMap,
+  ReactFlowProvider,
+  addEdge,
   useNodesState,
   useEdgesState,
+  Controls,
+  ReactFlowInstance,
+  Connection,
 } from 'reactflow';
-
 import 'reactflow/dist/style.css';
 
-const initNodes = [
-  {
-    id: 'a',
-    data: { label: 'Node A' },
-    position: { x: 250, y: 0 },
-  },
-  {
-    id: 'b',
-    data: { label: 'Node B' },
-    position: { x: 100, y: 100 },
-  },
-];
+import Sidebar from './Sidebar';
 
-const initEdges = [
+import './Flowchart.css';
+
+const initialNodes = [
   {
-    id: 'a-b',
-    source: 'a',
-    target: 'b',
+    id: '1',
+    type: 'input',
+    data: { label: 'input node' },
+    position: { x: 250, y: 5 },
   },
 ];
 
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 
-export default function Flowchart() {
-  const [nodes, , onNodesChange] = useNodesState(initNodes);
-  const [edges, , onEdgesChange] = useEdgesState(initEdges);
+const Flowchart = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance|null>(null);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
+  const onDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      // reactFlowInstance.project was renamed to reactFlowInstance.screenToFlowPosition
+      // and you don't need to subtract the reactFlowBounds.left/top anymore
+      // details: https://reactflow.dev/whats-new/2023-11-10
+      console.log("Instance: ", reactFlowInstance);
+      const position = reactFlowInstance!.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance],
+  );
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      onEdgesChange={onEdgesChange}
-      fitView
-    >
-      <Background />
-      <Controls />
-      <MiniMap />
-    </ReactFlow>
+    <div className="dndflow">
+      <ReactFlowProvider>
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </ReactFlowProvider>
+    </div>
   );
-}
+};
+
+export default Flowchart;
